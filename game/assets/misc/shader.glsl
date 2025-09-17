060c2df780b05fd429d0bab96ab7b0cf4511eb9b
Keyslam <justinvanderleij@gmail.com>
2025-09-17 21:39:05 +0200

    Fix shader for web

--- game/assets/misc/shader.glsl
+++ game/assets/misc/shader.glsl
@@ -1,28 +1,30 @@
 uniform Image palettes;
 uniform int pal;
-
 uniform bool flash;
 
-vec4 effect( vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords ) {
-    if (color.a == 0.f) {
+vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
+    if (color.a == 0.0) {
         discard;
     }
 
-	float step = 1.f / 9.f;
+    float step = 1.0 / 9.0;
 
     vec4 pixel = Texel(tex, texture_coords);
 
-    if (pixel.a == 0.f) {
+    if (pixel.a == 0.0) {
         discard;
     }
 
     float index = pixel.g;
 
     if (flash) {
-        index = 1.f;
+        index = 1.0;
     }
 
-    vec4 mapped = Texel(palettes, vec2(index, pal * step + step * 0.5));
+    // explicitly cast pal to float for WebGL
+    float palIndex = float(pal);
+
+    vec4 mapped = Texel(palettes, vec2(index, palIndex * step + step * 0.5));
 
     return mapped;
 }
